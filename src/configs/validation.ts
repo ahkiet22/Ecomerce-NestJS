568f35269e0c64ab0c2fb01fe1002bdcc7aa08b9
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-07-16 01:21:48 +0700

    feat: add module prisma, add validation envConfig

---
+++ src/configs/validation.ts
@@ -0,0 +1,36 @@
+import fs from 'fs'
+import path from 'path'
+import { config } from 'dotenv'
+import z from 'zod'
+
+config({
+  path: '.env',
+})
+
+// check file .env
+if (!fs.existsSync(path.resolve('.env'))) {
+  console.log('Not found file .env')
+  process.exit(1)
+}
+
+const configSchema = z.object({
+  DATABASE_URL: z.string(),
+  ACCESS_TOKEN_SECRET: z.string(),
+  ACCESS_TOKEN_EXPIRES_IN: z.string(),
+  REFRESH_TOKEN_SECRET: z.string(),
+  REFRESH_TOKEN_EXPIRES_IN: z.string(),
+  SECRET_API_KEY: z.string(),
+  BASE_URL_CLIENT: z.string(),
+})
+
+const configServer = configSchema.safeParse(process.env)
+
+if (!configServer.success) {
+  console.log('The declared values in the .evn file are invalid.')
+  console.log(configServer.error)
+  process.exit(1)
+}
+
+const envConfig = configServer.data
+
+export default envConfig
